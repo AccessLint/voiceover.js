4fd25d0590cf00561b46d3cfdccbaa2ba6d006a7
Cameron Cundiff <github@ckundo.com>
2022-04-02 13:46:20 -0400

    Add video screen capture support

---
+++ packages/voiceover/tests/example.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+import { startInteracting, VoiceOver } from "../lib";
+
+let voiceOver: VoiceOver;
+
+test.beforeAll(async () => {
+  voiceOver = new VoiceOver({ log: true });
+});
+
+test.beforeEach(async ({ page }) => {
+  voiceOver.record({ file: 'test-videos/recording.mov' });
+  await page.waitForTimeout(3000);
+  await voiceOver.launch();
+  await page.waitForTimeout(3000);
+  await page.goto("https://example.com");
+
+  await voiceOver.rotor({ menu: "Window Spots", find: "content" });
+  await voiceOver.execute(startInteracting);
+});
+
+test.afterEach(async () => {
+  await voiceOver.quit();
+});
+
+test.describe("example.com", () => {
+  test.setTimeout(30000);
+
+  test("link test", async () => {
+    const output = await voiceOver.advance({
+      target: { text: "More information..." },
+      steps: 5,
+    });
+
+    expect(output).toEqual("link More information...");
+  });
+});
